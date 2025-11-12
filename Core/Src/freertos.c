421642e343fa2ee48e1ec19503a4b76f26691acd
ayun1998-3 <77704153+ayun1998-3@users.noreply.github.com>
2025-11-12 02:56:11 -0500

    Created template RTOS project
    Deleted extraneous projects

---
+++ Core/Src/freertos.c
@@ -0,0 +1,207 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usart.h"
+#include "can.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticQueue_t osStaticMessageQDef_t;
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+/* USER CODE END Variables */
+/* Definitions for controlTask */
+osThreadId_t controlTaskHandle;
+const osThreadAttr_t controlTask_attributes = {
+  .name = "controlTask",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for feedbackTask */
+osThreadId_t feedbackTaskHandle;
+const osThreadAttr_t feedbackTask_attributes = {
+  .name = "feedbackTask",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for readADCTask */
+osThreadId_t readADCTaskHandle;
+const osThreadAttr_t readADCTask_attributes = {
+  .name = "readADCTask",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityBelowNormal,
+};
+/* Definitions for serialQueue */
+osMessageQueueId_t serialQueueHandle;
+uint8_t serialQueueBuffer[ 4 * 8 ];
+osStaticMessageQDef_t serialQueueControlBlock;
+const osMessageQueueAttr_t serialQueue_attributes = {
+  .name = "serialQueue",
+  .cb_mem = &serialQueueControlBlock,
+  .cb_size = sizeof(serialQueueControlBlock),
+  .mq_mem = &serialQueueBuffer,
+  .mq_size = sizeof(serialQueueBuffer)
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartControlTask(void *argument);
+void StartFeedbackTask(void *argument);
+void StartReadADCTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of serialQueue */
+  serialQueueHandle = osMessageQueueNew (4, 8, &serialQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of controlTask */
+  controlTaskHandle = osThreadNew(StartControlTask, NULL, &controlTask_attributes);
+
+  /* creation of feedbackTask */
+  feedbackTaskHandle = osThreadNew(StartFeedbackTask, NULL, &feedbackTask_attributes);
+
+  /* creation of readADCTask */
+  readADCTaskHandle = osThreadNew(StartReadADCTask, NULL, &readADCTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartControlTask */
+/**
+  * @brief  Function implementing the controlTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartControlTask */
+void StartControlTask(void *argument)
+{
+  /* USER CODE BEGIN StartControlTask */
+	// todo: Implement directControl() from "control.c" here
+
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartControlTask */
+}
+
+/* USER CODE BEGIN Header_StartFeedbackTask */
+/**
+* @brief Function implementing the feedbackTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartFeedbackTask */
+void StartFeedbackTask(void *argument)
+{
+  /* USER CODE BEGIN StartFeedbackTask */
+	// todo: implement writeToJetson() from "serial.c" here
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartFeedbackTask */
+}
+
+/* USER CODE BEGIN Header_StartReadADCTask */
+/**
+* @brief Function implementing the readADCTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartReadADCTask */
+void StartReadADCTask(void *argument)
+{
+  /* USER CODE BEGIN StartReadADCTask */
+	// todo: implement readPot() from "pot.c" here
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartReadADCTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
